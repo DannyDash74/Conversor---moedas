a18f79d41ed139c79fb4fff288a0e66ff89f8a90
DannyDash74 <danivan74velocross@gmail.com>
2025-09-26 18:03:28 -0300

    Add files via upload

---
+++ conversor - moedas pronto.py
@@ -0,0 +1,38 @@
+import requests
+
+def obter_taxas():
+    url = "https://api.exchangerate-api.com/v4/latest/USD"
+    response = requests.get(url)
+    data = response.json()
+    return data['rates']
+
+
+def converter_moedas(valor, moeda_origem, moeda_destino, taxas):
+    if moeda_origem in taxas and moeda_destino in taxas:
+        taxa_origem = taxas[moeda_origem]
+        taxa_destino = taxas[moeda_destino]
+        valor_em_usd = valor / taxa_origem
+        valor_convertido = valor_em_usd * taxa_destino
+        return valor_convertido
+    else:
+        return None
+
+API_KEY = "552f09aff92077dd07766766"
+
+taxa_de_cambio = obter_taxas()
+
+valor = float(input('Qual o valor a ser convertido: '))
+moeda_origem = input('Qual é a moeda de origem: ').upper()
+moeda_destino = input('Qual é a moeda destino: ').upper()
+
+valor_convertido = converter_moedas(
+    valor, moeda_origem, moeda_destino, taxa_de_cambio)
+
+print(f"{valor} {moeda_origem} equivalem a {valor_convertido:.2f} {moeda_destino}")
+
+
+if valor_convertido is not None:
+    print(f"\n{valor:.2f} {moeda_origem} equivalem a {valor_convertido:.2f} {moeda_destino}")
+else:
+    print("Moedas inválidas. Verifique os códigos informados.")
+    
